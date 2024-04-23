c59426e3ce9a74bc2c3f124b19a9a0dd4ad2cc86
Ho Kim <ho.kim@ulagbulag.io>
2024-04-23 13:04:56 +0000

    feat: add basic graph optimizer support

--- actions/dataset_optimize.py
+++ actions/dataset_optimize.py
@@ -1,5 +1,9 @@
+from typing import Optional
+import streamlit as st
+
 from assets import Assets
-from kubegraph.data.graph import NetworkGraph
+from kubegraph.data.graph import NetworkGraph, OptimalNetworkGraph
+from kubegraph.solver.ortools import OrToolsSolver
 from utils.types import DataModel, SessionReturn
 
 
@@ -10,7 +14,43 @@ async def render(assets: Assets, session: DataModel, name: str, spec: DataModel)
         value_type=NetworkGraph,
     )
 
+    optimized_graph = _draw_optimize(
+        name=name,
+        graph=graph,
+    )
+    if optimized_graph is None:
+        st.warning('Failed to optimize graph :(')
+        return {
+            'state': 'Empty',
+        }
+
     return {
         'state': 'Ok',
-        'value': graph,
+        'value': optimized_graph,
     }
+
+
+def _draw_optimize_network_flow(name: str, graph: NetworkGraph) -> Optional[OptimalNetworkGraph]:
+    smcf = OrToolsSolver.with_scalar_network_graph(graph)
+    return smcf.solve()
+
+
+def _draw_optimize(name: str, graph: NetworkGraph) -> Optional[OptimalNetworkGraph]:
+    # NOTE: Ordered
+    actions = {
+        'Network Flow': _draw_optimize_network_flow,
+    }
+
+    selected_action = st.selectbox(
+        key=f'{name}',
+        label='Choose one of optimizers',
+        options=actions.keys(),
+    )
+    if selected_action is None:
+        return st.stop()
+
+    with st.spinner('🔥 Optimizing...'):
+        return actions[selected_action](
+            name=f'{name}/{selected_action}',
+            graph=graph,
+        )
